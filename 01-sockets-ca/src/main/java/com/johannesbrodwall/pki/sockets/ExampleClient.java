e8f074759d5c93f8fcb4e91e9c931741d66d3e91
Johannes Brodwall <johannes@brodwall.com>
2020-03-03 01:28:44 +0100

    SSL Client and server (HACKY!)

--- 01-sockets-ca/src/main/java/com/johannesbrodwall/pki/sockets/ExampleClient.java
+++ 01-sockets-ca/src/main/java/com/johannesbrodwall/pki/sockets/ExampleClient.java
@@ -1,16 +1,55 @@
 package com.johannesbrodwall.pki.sockets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.management.PersistentMBean;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.util.Properties;
 
 public class ExampleClient {
-    public static void main(String[] args) throws IOException {
-        new ExampleClient().start();
+    private static final Logger logger = LoggerFactory.getLogger(ExampleClient.class);
+    private final Properties properties = new Properties();
+
+    public ExampleClient(String filename) throws IOException {
+        try (FileReader reader = new FileReader(filename)) {
+            properties.load(reader);
+        }
     }
 
-    private void start() throws IOException {
-        Socket socket = new Socket("localhost", 30001);
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        new ExampleClient("local-sockets.properties").start();
+    }
+
+    private void start() throws IOException, GeneralSecurityException {
+        KeyStore keyStore = KeyStore.getInstance("pkcs12");
+        keyStore.load(new FileInputStream(properties.getProperty("client.keystore.filename")), properties.getProperty("client.keystore.password").toCharArray());
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, properties.getProperty("client.key.password").toCharArray());
+
+        KeyStore trustStore = KeyStore.getInstance("pkcs12");
+        trustStore.load(new FileInputStream(properties.getProperty("ca.keystore.filename")), properties.getProperty("ca.keystore.password").toCharArray());
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(trustStore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+        InetSocketAddress serverAddress = InetSocketAddress.createUnresolved("localhost", 30001);
+        logger.info("Connecting to {}", serverAddress);
+
+        Socket socket = sslContext.getSocketFactory().createSocket(serverAddress.getHostName(), serverAddress.getPort());
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         socket.getInputStream().transferTo(buffer);
         System.out.println(new String(buffer.toByteArray()));
--- 01-sockets-ca/src/main/java/com/johannesbrodwall/pki/sockets/ExampleServer.java
+++ 01-sockets-ca/src/main/java/com/johannesbrodwall/pki/sockets/ExampleServer.java
@@ -1,25 +1,74 @@
 package com.johannesbrodwall.pki.sockets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
 
 public class ExampleServer {
+    private static final Logger logger = LoggerFactory.getLogger(ExampleServer.class);
 
-    public static void main(String[] args) throws IOException {
-        new ExampleServer().start();
+    private Properties properties = new Properties();
+
+    public ExampleServer(String filename) throws IOException {
+        try (FileReader reader = new FileReader(filename)) {
+            properties.load(reader);
+        }
     }
 
-    private void start() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(30001);
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        new ExampleServer("local-sockets.properties").start();
+    }
+
+    private void start() throws IOException, GeneralSecurityException {
+        KeyStore keyStore = KeyStore.getInstance("pkcs12");
+        keyStore.load(new FileInputStream(properties.getProperty("server.keystore.filename")), properties.getProperty("server.keystore.password").toCharArray());
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, properties.getProperty("server.key.password").toCharArray());
+
+        KeyStore trustStore = KeyStore.getInstance("pkcs12");
+        trustStore.load(new FileInputStream(properties.getProperty("ca.keystore.filename")), properties.getProperty("ca.keystore.password").toCharArray());
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(trustStore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+        SSLServerSocket serverSocket = (SSLServerSocket) sslContext.getServerSocketFactory().createServerSocket(30001);
+        serverSocket.setWantClientAuth(true);
         while (!Thread.interrupted()) {
+            logger.info("Waiting for connections: {}", serverSocket.getLocalSocketAddress());
             try (Socket clientSocket = serverSocket.accept()) {
+                logger.info("Connected: {}", clientSocket);
                 handleClient(clientSocket);
+            } catch (IOException e) {
+                logger.error("Failed to handle socket", e);
             }
         }
     }
 
     private void handleClient(Socket clientSocket) throws IOException {
-        clientSocket.getOutputStream().write("Hello world".getBytes());
+        String peer = "world";
+        if (clientSocket instanceof SSLSocket) {
+            SSLSocket sslSocket = (SSLSocket) clientSocket;
+            Certificate[] certificates = sslSocket.getSession().getPeerCertificates();
+            logger.info("{} certificates: {}", "Peer", certificates);
+            peer = ((X509Certificate)certificates[0]).getSubjectDN().getName();
+        }
+        logger.info("Responding");
+        clientSocket.getOutputStream().write(("Hello " + peer).getBytes());
     }
 }
